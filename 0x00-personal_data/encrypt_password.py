1fcdb1a5aedb87f52414481bef7d0c3297549e8d
rnpacifique <rpacfique12@gmail.com>
2024-07-04 20:22:32 +0200

    Implement a hash_password function that expects one string argument name password and returns a salted, hashed password, which is a byte string.

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""This function hashes a password using bcrypt"""
+
+
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """Hashes a password using bcrypt."""
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+    return hashed_password
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """Validates a password against a hashed password."""
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
+
+
+if __name__ == "__main__":
+    password = "MyAmazingPassw0rd"
+    encrypted_password = hash_password(password)
+    print(encrypted_password)
+    print(is_valid(encrypted_password, password))
